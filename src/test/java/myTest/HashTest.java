9a35d46cb6a2d7b2a259afe4ed827f78a41f2f03
jianan1104 <644219889@qq.com>
2018-04-27 23:04:57 +0800

    add hashTest

--- src/test/java/myTest/HashTest.java
+++ src/test/java/myTest/HashTest.java
@@ -1,48 +1,65 @@
 package myTest;
 
 import java.util.HashMap;
+import java.util.Hashtable;
 
-public class HashTest implements Runnable {
+public class HashTest {
+    static Hashtable<Integer, Integer> table = new Hashtable<>();
+    //分别在两个子线程内对hashtable进行put,get操作
+//    public static void main(String[] args) {
+//        new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                // TODO Auto-generated method stub
+//                System.out.println("线程1执行");
+//                for (int i = 0; i < 1000; i++) {
+//                    table.put(i,i);
+//                }
+//                System.out.println("线程1---------" + table.get(500));
+//            }
+//        }).start();
+//        new Thread(new Runnable() {
+//
+//            @Override
+//            public void run() {
+//                // TODO Auto-generated method stub
+//                System.out.println("线程2执行");
+//                for (int i = 1000; i < 2000; i++) {
+//                    table.put(i, i);
+//                }
+//                System.out.println("线程2---------" + table.get(1500));
+//            }
+//        }).start();
+//    }
 
-    private HashMap<String, Integer> h = new HashMap<String, Integer>();
 
-    public HashTest() {
-        h.put("1", 0);
-    }
-
-    @Override
-    public void run() {
-
-        h.put("1", h.get("1") + 1);
-
-    }
+    static HashMap<Integer, Integer> map = new HashMap<>();
+    //分别在两个子线程内对hashmap进行put,get操作
     public static void main(String[] args) {
-        HashTest h = new HashTest();
 
-        for(int i=1; i<=1000; i++) {
-            Thread t = new Thread(h);
-            t.start();
-        }
+        new Thread(new Runnable() {
 
-        System.out.println(h.getH().get("1"));
+            @Override
+            public void run() {
+                // TODO Auto-generated method stub
+                System.out.println("线程1执行");
+                for (int i = 0; i < 1000; i++) {
+                    map.put(i,i);
+                }
+                System.out.println("线程1---------" + map.get(500));
+            }
+        }).start();
+        new Thread(new Runnable() {
 
-        String a = "a";
-        String b = "a";
-        HashMap<String, Integer> hashMap = new HashMap<String, Integer>();
-
-        hashMap.put(a,1);
-        hashMap.put(b,2);
-        System.out.println(hashMap.get(a));
-
-
-    }
-
-
-    public void setH(HashMap<String, Integer> h) {
-        this.h = h;
-    }
-
-    public HashMap<String, Integer> getH() {
-        return h;
+            @Override
+            public void run() {
+                // TODO Auto-generated method stub
+                System.out.println("线程2执行");
+                for (int i = 1000; i < 2000; i++) {
+                    map.put(i, i);
+                }
+                System.out.println("线程2---------" + map.get(1500));
+            }
+        }).start();
     }
 }
