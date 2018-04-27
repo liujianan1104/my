802b98092febace0a961237c4f210b6c96dc59c4
liujianan <644219889@qq.com>
2018-04-27 14:52:33 +0800

    修改压缩文件

--- src/main/java/mywork/my/myUtil/ZipUtils.java
+++ src/main/java/mywork/my/myUtil/ZipUtils.java
@@ -1,9 +1,15 @@
 package mywork.my.myUtil;
 
-import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.util.CharsetUtil;
 import cn.hutool.core.util.ZipUtil;
+import cn.hutool.log.Log;
+import cn.hutool.log.LogFactory;
+import groovy.util.CharsetToolkit;
+import org.springframework.util.StringUtils;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
 
 /**
  * User: Liu Jianan
@@ -12,32 +18,73 @@ import java.io.File;
  */
 public class ZipUtils {
 
-    public void getGZFile(String inputFileName, String outFilePath) {
-        File inputFile = new File(inputFileName);
-        String targetFileName = "";
+    private static final Log logger = LogFactory.get();
 
-        if (inputFile.isDirectory()) { //获得打包文件夹名
-            targetFileName = inputFileName.substring(inputFileName.lastIndexOf(File.separator) + 1) + ".tar.gz";
-            zipFiles(inputFile,outFilePath + File.separator + targetFileName);
-        } else { //获得打包文件名
-            targetFileName = inputFileName.substring((inputFileName.lastIndexOf(File.separator) + 1), (inputFileName.lastIndexOf("."))) + ".tar.gz";
-            zipFile(inputFile, outFilePath + File.separator + targetFileName);
-        }
+    /**
+     * 压缩文件或文件夹
+     *
+     * @param inputFileName 源文件或文件夹路径（绝对路径）
+     * @throws Exception
+     */
+    public void getGZFile(String inputFileName) throws Exception {
+        logger.info("Compression start...");
 
-    }
+        try {
+            logger.info("inputFileName:" + inputFileName);
 
-    private void zipFile(File inputFile, String outFileName) {
-        ZipUtil.zip(inputFile.getAbsolutePath(), outFileName);
-    }
+            File inputFile = new File(inputFileName);
+            String targetFileName = ""; //压缩文件名
 
-    private void zipFiles(File inputFile, String outFileName) {
-        File[] fileList = inputFile.listFiles();
-
-        if (null != fileList && fileList.length > 0) {
-            for (File f : fileList) {
-                zipFile(f, outFileName);
+            if (inputFile.isDirectory()) { //获得打包文件夹名
+                targetFileName = inputFileName.substring(inputFileName.lastIndexOf(File.separator) + 1) + ".tar.gz";
+                logger.info("targetFileName:" + inputFile.getParent() + File.separator + targetFileName);
+                String charset = "";
+                File[] files = inputFile.listFiles();
+                for (File f : files) {
+                    charset = getCharSet(f); //获得文件编码格式
+                }
+                zipFile(inputFileName, inputFile.getParent() + File.separator + targetFileName, charset); //压缩文件件
+            } else { //获得打包文件名
+                targetFileName = inputFileName.substring(0, inputFileName.lastIndexOf(".")) + ".tar.gz";
+                logger.info("targetFileName:" + targetFileName);
+                String charset = getCharSet(inputFile); //获得文件编码格式
+                zipFile(inputFileName, targetFileName, charset); // 压缩文件
             }
+
+            logger.info("Compression success end...");
+        } catch (Exception e) {
+            logger.error("Compression failed ! " + e.getMessage());
+            throw e;
         }
+    }
+
+    /**
+     * 压缩文件或文件夹
+     *
+     * @param inputFile   源文件或文件夹路径
+     * @param outFileName
+     * @param charset
+     */
+    private void zipFile(String inputFile, String outFileName, String charset) {
+        if (charset.isEmpty()) {
+            charset = CharsetUtil.UTF_8;
+        }
+        ZipUtil.zip(inputFile, outFileName, Charset.forName(charset), true);
+    }
+
+    /**
+     * 获得文件编码
+     *
+     * @param inputFile 源文件
+     * @return 文件编码
+     * @throws IOException
+     */
+    public String getCharSet(File inputFile) throws IOException {
+        CharsetToolkit detector = new CharsetToolkit(inputFile);
+        detector.setDefaultCharset(Charset.forName(CharsetUtil.GBK));//对无法解析的都使用期默认的编码,故要设置默认编码
+        Charset charset = detector.getCharset();
+        logger.info("inputFile: " + inputFile.getName() + ". Charset: " + charset.name());
+        return charset.name();
     }
 
 }
--- src/test/java/myTest/Test.java
+++ src/test/java/myTest/Test.java
@@ -1,15 +1,70 @@
 package myTest;
 
+import groovy.util.CharsetToolkit;
 import mywork.my.myUtil.ZipUtils;
 
-import java.io.IOException;
+import java.io.File;
+import java.nio.charset.Charset;
 
 public class Test {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
+//        Charset c = Charset;
 
         ZipUtils m = new ZipUtils();
 
-        m.getGZFile("G:\\test","G:\\test");
+        m.getGZFile("D:\\testFile\\1.csv");
+
+//        File file = new File("D:\\testFile\\1.csv");
+//            CharsetToolkit detector = new CharsetToolkit(file);
+//            detector.setDefaultCharset(Charset.forName("GBK"));
+//            Charset charset = detector.getCharset();
+//            System.out.println(charset.name());
+//        String fileContent=FileUtils.readFileToString(new File(filePath),fileEncode);
+
+//        InputStream inputStream  = new FileInputStream("D:\\testFile\\tar\\1.txt");
+//        byte[] head = new byte[3];
+//
+//        inputStream.read(head);
+//        inputStream.close();
+//        System.out.println(head);
+//
+//        String code = "";
+//
+//        code = "gb2312";
+//
+//        if (head[0] == -1 && head[1] == -2 )
+//
+//            code = "UTF-16";
+//
+//        if (head[0] == -2 && head[1] == -1 )
+//
+//            code = "Unicode";
+//
+//        if(head[0]==-17 && head[1]==-69 && head[2] ==-65)
+//
+//            code = "UTF-8";
+//
+//        System.out.println(code);
+
+//        CodepageDetectorProxy detector = CodepageDetectorProxy.getInstance();
+//        detector.add(new ParsingDetector(false));
+//        detector.add(JChardetFacade.getInstance());// 用到antlr.jar、chardet.jar
+//        // ASCIIDetector用于ASCII编码测定
+//        detector.add(ASCIIDetector.getInstance());
+//        // UnicodeDetector用于Unicode家族编码的测定
+//        detector.add(UnicodeDetector.getInstance());
+//        java.nio.charset.Charset charset = null;
+//        File f = new File("D:\testFile\\tar\\bid_detail_single_src_ggzy.weifang.gov.cn.json");
+//        try {
+//            charset = detector.detectCodepage(f.toURI().toURL());
+//            System.out.println(charset);
+//        } catch (Exception ex) {
+//            ex.printStackTrace();
+//        }
+
+
+
+//        ZipUtil.zip("D:\\testFile\\tar", "D:\\testFile\\a.tar.gz", Charset.forName("utf-8"), true);
 //        TarUtilsbak tarUtils = new TarUtilsbak();
 //        tarUtils.execute("D:\\testFile\\tar\\bid_detail_single_src_ggzy.weifang.gov.cn.json","D:\\testFile\\tar");
 //        System.out.println(1);
@@ -21,7 +76,7 @@ public class Test {
 //        System.out.println(s.substring((s.lastIndexOf("\\") + 1),(s.lastIndexOf("."))));
 
 //        String in = "住房租赁和【销售管理条例】POS公开征求意见  解读_要闻_【中标国网】ORG";
-//
+//w
 //        HashMap<String, String> param = new HashMap<String, String>();
 //        param.put("【([^【】]*)】ORG","ORG");
 //        param.put("【(.+?)】POS","POS");
